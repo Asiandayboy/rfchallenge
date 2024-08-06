4b1408cf356a66c68be9c5307ac7bf2f662d57e7
Asiandayboy <russellcheeseday@gmail.com>
2024-08-05 21:21:53 -0400

    initial commit

---
+++ src/components/Transactions/index.tsx
@@ -0,0 +1,43 @@
+import { useCallback } from "react"
+import { useCustomFetch } from "src/hooks/useCustomFetch"
+import { SetTransactionApprovalParams } from "src/utils/types"
+import { TransactionPane } from "./TransactionPane"
+import { SetTransactionApprovalFunction, TransactionsComponent } from "./types"
+
+export const Transactions: TransactionsComponent = ({ transactions }) => {
+  const { fetchWithoutCache, loading, clearCacheByEndpoint } = useCustomFetch()
+
+  const setTransactionApproval = useCallback<SetTransactionApprovalFunction>(
+    async ({ transactionId, newValue }) => {
+      await fetchWithoutCache<void, SetTransactionApprovalParams>("setTransactionApproval", {
+        transactionId,
+        value: newValue,
+      })
+
+      /*
+        bug #7 -> my solution: clear the cache for these endpoints so that they don't retrieve the 
+        old transactions with the old "approved" state. The next time a request is made to these endpoints, 
+        they'll fetch the new transactions and then cache that. 
+      */
+      clearCacheByEndpoint(["paginatedTransactions", "transactionsByEmployee"])
+    },
+    [fetchWithoutCache]
+  )
+
+  if (transactions === null) {
+    return <div className="RampLoading--container">Loading...</div>
+  }
+
+  return (
+    <div data-testid="transaction-container">
+      {transactions.map((transaction) => (
+        <TransactionPane
+          key={transaction.id}
+          transaction={transaction}
+          loading={loading}
+          setTransactionApproval={setTransactionApproval}
+        />
+      ))}
+    </div>
+  )
+}
